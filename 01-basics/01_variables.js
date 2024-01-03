16673483dbc34ccdd6efb1d36f6905b91d974c83
AryanBh7631 <155455129+AryanBh7631@users.noreply.github.com>
2024-01-03 02:25:53 +0000

    changes

--- 01-basics/01_variables.js
+++ 01-basics/01_variables.js
@@ -3,5 +3,13 @@ let accountEmail = "aryanbhagat7631@gmail.com"
 var accountPassword = "12345"
 accountCity = "Jalandhar"
 // accountId = 3278
+accountEmail = "hiteshcrook@gmail.com"
+accountPassword = "23rt3"
+let accountState;
 
-console.log(accountId);
+// console.log(accountId);
+// console.log(accountEmail);
+// console.log(accountPassword);
+console.table([accountId, accountCity, accountEmail, accountPassword, accountState])
+
+
