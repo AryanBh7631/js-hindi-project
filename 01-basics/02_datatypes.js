d3484867ca3d6eed5acb967e93569535b917a778
AryanBh7631 <155455129+AryanBh7631@users.noreply.github.com>
2024-01-03 14:28:14 +0000

    new change

--- 01-basics/02_datatypes.js
+++ 01-basics/02_datatypes.js
@@ -2,8 +2,22 @@
 
 //  alert( 3 + 3 ) we are using node js not browser
 
-console.log(3 + 3)
+console.log(4 + 3)
 
 console.log("Hitesh")
+
+let name = "aryan"
+let age = 22
+let isLoggedIn = false
+
+let state = null
+let accent = ""
+
+console.log(typeof "aryan");
+console.log(typeof 22);
+console.log(typeof false);
+console.log(typeof null);
+console.log(typeof undefined);
+
 
 
