a69e736e4ac8cfbf678a0a92d0ae34612722860a
AryanBh7631 <155455129+AryanBh7631@users.noreply.github.com>
2024-01-03 15:11:52 +0000

    commit

---
+++ 01-basics/03_conversionoperation.js
@@ -0,0 +1,34 @@
+let score = "aryan"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+let newnumber = "aryan"
+
+let booleanchange = Boolean(newnumber)
+// console.log(booleanchange);
+// console.log(typeof booleanchange);
+
+let ageGroup = 22
+let stringValue = String(ageGroup)
+
+// console.log(stringValue);
+// console.log(typeof stringValue);
+
+// ******************* Operations *************************************
+
+// console.log(2 + 2);
+
+let str1 = "hello"
+let str2 = " aryan"
+
+let str3 = str1 + str2
+
+console.log(str1 + " str2");
+console.log(str3);
+
+console.log(true+);
