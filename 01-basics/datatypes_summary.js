03b7ff68170d16af89f29fb2c74eb64f83e56638
AryanBh7631 <155455129+AryanBh7631@users.noreply.github.com>
2024-01-04 14:40:24 +0000

    change

--- 01-basics/datatypes_summary.js
+++ 01-basics/datatypes_summary.js
@@ -1,9 +1,24 @@
 const id = Symbol('123')
 const anotherId = Symbol('123')
 
-console.log(id);
-console.log(anotherId);
-console.log(id == anotherId);
+// console.log(id);
+// console.log(anotherId);
+// console.log(id == anotherId);
 
 const bigNumber = 324564664468678n
-console.log(bigNumber);
+// console.log(bigNumber);
+
+
+// Array Object Function
+
+const arr1 = ["shiva", "ram", "krishna"];
+let myobj = {
+    name: "aryan",
+    age: 22,
+}
+const myfunction = function () {
+        console.log("hello world");
+}
+console.log(typeof arr1);
+console.log(typeof myobj);
+console.log(typeof myfunction);
