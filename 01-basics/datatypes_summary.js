cd6e70d00c8c4f314e248391f8b9c38bc4740e2a
AryanBh7631 <155455129+AryanBh7631@users.noreply.github.com>
2024-01-04 02:25:25 +0000

    timechange

---
+++ 01-basics/datatypes_summary.js
@@ -0,0 +1,9 @@
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id);
+console.log(anotherId);
+console.log(id == anotherId);
+
+const bigNumber = 324564664468678n
+console.log(bigNumber);
