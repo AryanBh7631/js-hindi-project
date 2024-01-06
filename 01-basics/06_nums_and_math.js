08e3ffbba9116779893be33f360f3f1dca1a4ded
AryanBh7631 <155455129+AryanBh7631@users.noreply.github.com>
2024-01-06 15:11:45 +0000

    save asap

--- 01-basics/06_nums_and_math.js
+++ 01-basics/06_nums_and_math.js
@@ -1,18 +1,48 @@
 const score = 400
-console.log(score);
+// console.log(score);
 
 const balance = new Number(100)
-console.log(balance);
 
-console.log(balance.toString().length);
+// console.log(balance);
 
-console.log(balance.toFixed(1));
+// console.log(balance.toString().length);
+
+// console.log(balance.toFixed(1));
 
 const otherNumber = 123.8966
 
-console.log(otherNumber.toPrecision(4));
+// console.log(otherNumber.toPrecision(3));
 
 const hundreds = 100000
 
-console.log(hundreds.toLocaleString('en-In'));
+// console.log(hundreds.toLocaleString('en-In'));
+
+const sync = new String('aryan')
+// console.log(sync);
+
+const numero = 245
+const string1 = Boolean(numero)
+// console.log(string1);
+// console.log(typeof string1);
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6));
+// console.log(Math.round(4.2));
+// console.log(Math.ceil(4.1));
+// console.log(Math.floor(4.9));
+// console.log(Math.min(4, 6, 2, 8));
+// console.log(Math.max(4, 6, 2, 8));
+// console.log((Math.random() * 10 + 1));
+// console.log(Math.floor(Math.random() * 10 + 1));
+const min = 10
+const max = 20
+
+// console.log(Math.floor(Math.random() * (max - min + 1)) + min);
+// console.log(Math.floor(Math.random() * (11)) + min);
+
+console.log(Math.floor(Math.random() * 11));
+
+
+
 
