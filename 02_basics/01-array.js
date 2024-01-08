879f20b69ea6a255ad35f0a67ed2027d32a89560
AryanBh7631 <155455129+AryanBh7631@users.noreply.github.com>
2024-01-08 15:12:20 +0000

    save soon

--- 02_basics/01-array.js
+++ 02_basics/01-array.js
@@ -1,2 +1,44 @@
 const myArr = [0, 1, 2, 3, 4, 5]
-console.log(typeof myArr);
+
+// console.log(typeof myArr);
+// console.log(myArr[2]);
+
+myArr.push(6)
+// console.log(myArr);
+
+myArr.push(7)
+// console.log(myArr);
+
+myArr.pop()
+// console.log(myArr);
+
+myArr.unshift(9)
+// console.log(myArr);
+
+// myArr.shift()
+// console.log(myArr);
+
+// console.log(myArr.includes(9));
+// console.log(myArr.indexOf(9));
+// console.log(myArr.indexOf(10));
+
+// console.log(myArr.join());
+// console.log(myArr.slice(0, 3));
+// console.log(myArr.join());
+
+// console.log(myArr.splice(0, 3));
+// console.log(myArr.join());
+
+const marvelHeroes = ["ironman", "superman", "batman"]
+const dcHeroes = ["blackjack", "loki", "thor"]
+// console.log(typeof marvelHeroes);
+// console.log(typeof dcHeroes);
+
+const allHeroes = marvelHeroes.concat(dcHeroes)
+console.log(allHeroes);
+
+const newHeroes = [...marvelHeroes,...dcHeroes]
+console.log(newHeroes);
+
+
+
