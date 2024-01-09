91e729e32942656404935affd944363f2eda272a
AryanBh7631 <155455129+AryanBh7631@users.noreply.github.com>
2024-01-09 15:15:19 +0000

    same copy

--- 02_basics/03_object.js
+++ 02_basics/03_object.js
@@ -1,13 +1,44 @@
 const mySum = Symbol("key1")
+// console.log(typeof mySum);
 
 const jsUser = {
-    name : "aryan"
-    age : 22
-    location: madhepura
-    email: aryanbhagat7631@gmail.com
-    isLoggedIn: false
+    name : "aryan",
+    "full name": "Aryan Bhagat",
+    [mySum]: "mykey1",
+    age : 22,
+    location: "madhepura",
+    email: "aryanbhagat7631@gmail.com",
+    isLoggedIn: false,
     lastLoggedInDays: ['monday', 'tuesday'] 
 }
-console.log(jsUser.email);
-console.log(jsUser["email"]);
+// console.log(jsUser.email)
+// console.log(jsUser["email"])
+// console.log(jsUser['full name']);
+// console.log(typeof jsUser[mySum]);
+
+jsUser.email = 'aaaa@gmail.com'
+// Object.freeze(jsUser)
+
+// console.log(jsUser["email"]);
+// console.log(jsUser);
+
+jsUser.greeting = function () {
+    console.log("Hello Js User");
+}
+console.log(jsUser.greeting());
+
+jsUser.greetingTwo = function(){
+    console.log(`Hello Js user ${this.name}`);
+}
+console.log(jsUser.greetingTwo());
+
+
+
+
+
+
+
+
+
+
 
---
+++ 02_basics/04_object.js
@@ -0,0 +1,30 @@
+// const tinderUser = new Object()
+// const tinderUser1 = Object()
+// console.log(tinderUser1);
+
+const tinderUser = {}
+
+tinderUser.name = "aryan bhagat"
+tinderUser.email = "aryan@gmail.com"
+
+// console.log(tinderUser);
+
+const randomUser = {
+    email: "light@gmail.com",
+    fullname: {
+        userfullname:{
+            firstname: 'aryan',
+            lastname: 'bhagat'
+        }
+    }
+}
+
+console.log(randomUser.fullname.userfullname.firstname);
+
+
+const obj1 = { 1: "a" 2:"b"}
+const obj2 = {3:"c" 4:"d"}
+
+
+
+
