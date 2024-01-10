fedb51aff45c3ab58363fcbb135de8b1e681ea91
AryanBh7631 <155455129+AryanBh7631@users.noreply.github.com>
2024-01-10 15:16:17 +0000

    json

---
+++ 03_basics/01_function.js
@@ -0,0 +1,19 @@
+function sayMyName(){
+    console.log("A");
+    console.log("R");
+    console.log("Y");
+    console.log("A");
+    console.log("N");
+}
+sayMyName()
+
+
+
+function addTwoNumbers(number1, number2){
+    return number1 + number2
+}
+
+console.log(addTwoNumbers(3, 5));
+
+
+
