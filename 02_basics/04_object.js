b70bfffac624d5f19fe8535caf3a37d175616737
AryanBh7631 <155455129+AryanBh7631@users.noreply.github.com>
2024-01-10 06:30:42 +0000

    save em

--- 02_basics/04_object.js
+++ 02_basics/04_object.js
@@ -19,11 +19,68 @@ const randomUser = {
     }
 }
 
-console.log(randomUser.fullname.userfullname.firstname);
+// console.log(randomUser.fullname.userfullname.firstname);
+
+const obj1 = { 1: "a", 2:"b"}
+const obj2 = { 3: "c", 4:"d"}
+
+const obj3 = {...obj1, ...obj2}
+// console.log(obj3);
+
+const obj4 = Object.assign({}, obj1, obj2)
+// console.log(obj4);
+
+const obj5 = Object.assign(obj1, obj2)
+// console.log(obj1);
 
 
-const obj1 = { 1: "a" 2:"b"}
-const obj2 = {3:"c" 4:"d"}
+let user = [
+    {
+        id: "hitesh",
+        pass: "123"
+    },
+    {
+        id: "hitesh",
+        pass: "456"
+    },
+    {
+        id: "hitesh",
+        pass: "789"
+    }
+]
+
+// console.log(user[1].pass);
+// console.log(Object.keys(randomUser));
+// console.log(Object.values(randomUser));
+// console.log(Object.entries(randomUser));
+
+// console.log(randomUser.hasOwnProperty("email"));
+
+const course = {
+    coursename: "js in hindi",
+    price: "999",
+    courseInstructor: "aryan_king"
+
+}
+
+console.log(course.courseInstructor);
+
+const {courseInstructor: instructor} = course
+// console.log(courseInstructor);
+console.log(instructor);
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
