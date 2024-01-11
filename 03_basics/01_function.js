d021ab745c1f922e2de9ea8122aa2b24c945aff0
AryanBh7631 <155455129+AryanBh7631@users.noreply.github.com>
2024-01-11 06:32:31 +0000

    save

--- 03_basics/01_function.js
+++ 03_basics/01_function.js
@@ -7,13 +7,30 @@ function sayMyName(){
 }
 sayMyName()
 
-
-
 function addTwoNumbers(number1, number2){
     return number1 + number2
 }
 
 console.log(addTwoNumbers(3, 5));
+
+function multiply(number4, number5) {
+    return number4 * number5
+}
+
+let user = multiply(2,3)
+console.log(user);
+
+function logInUserMessage(username) {
+    if (username === undefined) {
+        console.log("Please enter a name");
+        return
+    }
+    return `${username} just logged in`
+}
+
+console.log(logInUserMessage());
+
+
 
 
 
