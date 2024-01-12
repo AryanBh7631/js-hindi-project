c08e5f34a935061c966390d6110bd1f1c1c9e984
AryanBh7631 <155455129+AryanBh7631@users.noreply.github.com>
2024-01-12 06:30:08 +0000

    savee

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -1,5 +1,4 @@
 var c = 200
-
 let a = 3000
 
 if (true) {
@@ -7,14 +6,58 @@ if (true) {
     const b = 20
     // c = 20 (this type of value also comes out of scope)
     var c  = 30
-    console.log("INNER : ", a);
+    // console.log("INNER : ", a);
 }
-
 
 // console.log(a);
 // console.log(b);
+// console.log(c);
 
-console.log(c);
+function one() {
+    const username = "hitesh"
 
+    function two() {
+        const website = "youtube"
+        console.log(username +  website);
+    }
+    // console.log(website);
 
+    two()
+}
+// one()
 
+if (true) {
+    const username = "hitesh"
+    if (username === "hitesh") {
+        const website = "youtube"
+        // console.log(username + " " + website);
+    }
+}
+// console.log(username);
+
+// console.log(addone(9));
+
+function addone(num1) {
+    return num1 + 1
+}
+
+// console.log(addtwo(2));
+
+const addtwo = function(num1){
+    return num1 + 2
+}
+
+const user = {
+    username: "hitesh",
+    price: "999",
+    
+    welcomeMessage: function () {
+        console.log(`${this.username} , welcome to website`);
+        console.log(this);
+    }
+    
+}
+user.welcomeMessage()
+user.username = "sad"
+user.welcomeMessage()
+console.log(this);
