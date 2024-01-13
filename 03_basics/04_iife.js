04569f3864771f101382080b82692897e30375e2
AryanBh7631 <155455129+AryanBh7631@users.noreply.github.com>
2024-01-13 15:10:30 +0000

    do save

--- 03_basics/04_iife.js
+++ 03_basics/04_iife.js
@@ -1,3 +1,18 @@
 (function chai() {
     console.log(`DB CONNECTED`);
-})()
+})();
+
+
+(() => {
+    console.log(`Green`);
+})();
+
+((name) => {
+    console.log(`Green ${name}`);
+})("ARYAN")
+
+
+// (let one = function name(params) {
+//     console.log(`BLUE IS THE COLOUR`);
+// })
+
