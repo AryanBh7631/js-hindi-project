e42ca91ab2f9acbc66d31116b5602c88d84cb8c0
AryanBh7631 <155455129+AryanBh7631@users.noreply.github.com>
2024-01-15 15:11:47 +0000

    time over

--- 04_control_flow/one.js
+++ 04_control_flow/one.js
@@ -1,14 +1,122 @@
-const isUserLoggedIn = true
+// const isUserLoggedIn = true
 
-if (isUserLoggedIn) {
+// if (isUserLoggedIn) {
     
-}
+// }
 
-if (false) {
+// if (false) {
     
-}
-if (2 === "2") {
-    const location = "lakshwdweep"
-    console.log(`We are going to ${location}`);
-}
+// }
+// if (2 === "2") {
+//     const location = "lakshwdweep"
+//     console.log(`We are going to ${location}`);
+// }
+
+// if (3 < 2) {
+//     const location = "lakshwdweep"
+//     console.log(`We are going to ${location}`);
+// }
+
+// if (3 != 2) {
+//     const location = "lakshwdweep"
+//     console.log(`We are going to ${location}`);
+// }
+
+// const score = 200
+
+// if (score > 100) {
+//     const power = "fly"
+//     console.log(`User power: ${power}`);
+// }
+
+// console.log(`User power: ${power}`);
+
+
+// const balance = 400
+
+// if (balance < 500) {
+//     console.log("User");
+// }
+// else{
+//     console.log("does not exist");
+// }
+
+
+// const balance = 1000
+
+// if (balance < 500) {
+//     console.log("less than 500");
+
+// }else if (balance < 750) {
+//     console.log("less than 750");
+
+// }else if (balance < 1000) {
+//     console.log("less than 1000");
+
+// }else{
+//     console.log("loose");
+
+// }
+
+// const userloggedIn = true
+// const userEmail = false
+
+// if (userloggedIn && userEmail && 2 == 2) {
+//     console.log("true");
+// }
+// if (userloggedIn || userEmail ) {
+//     console.log("false");
+// }
+
+// let month = 20
+
+// switch (month) {
+//     case 10:
+//         console.log("one");
+//         break;
+//     case 20:
+//         console.log("two");
+//         break;
+//     case 25:
+//         console.log("three");
+//         break;
+
+//     default:
+//         console.log("default");
+//         break;
+// }
+
+// if (1) {
+//     console.log("one");
+// }
+
+// if (-0) {
+//     console.log("one");
+// }
+
+// let array = ["superman", "shaktiman"]
+// console.log(array.length);
+// if (array.length === 3) {
+//     console.log("array is one");
+// }
+
+// const emptyobj = {
+//     // name: "aryan"
+// }
+// console.log(Object.keys(emptyobj));
+
+// if (Object.keys(emptyobj).length===0) {
+//     console.log("object");
+// }
+
+let val1;
+
+val1 = 5 ?? 10
+
+console.log(val1);
+
+val1 = undefined ?? 10
+
+console.log(val1);
+
 
