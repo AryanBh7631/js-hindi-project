e3a7d8ac3ef981a38bd05ae82b1104f259ac15a3
AryanBh7631 <155455129+AryanBh7631@users.noreply.github.com>
2024-01-16 15:12:31 +0000

    save and commit

--- 05_iterations/one.js
+++ 05_iterations/one.js
@@ -1,13 +1,71 @@
 // for
 
-for (let title = 1; title < 10; title++) {
-    const element = title;
-    if (title = 5) {
-        console.log("5 detected");
-    }
-    console.log(element);
+// for (let title = 1; title < 10; title++) {
+//     const element = title;
+//     if (title = 5) {
+//         console.log("5 detected");
+//     }
+//     console.log(element);
  
+// }
+
+// for (let index = 0; index < 10; index++) {
+//     const element = index;
+//     if (index = 5) {
+//         console.log("5 detected");
+//     }
+//     console.log(element);
+
+// }
+
+// for (let a = 0; a < 10; a++) {
+
+//     const element = a;
+//     console.log(`Inner element ${element}`);
+
+//     for (let j = 0; j < 10; j++) {
+//         const outisde = j;
+//         // console.log(`Outside element ${outisde} and inner element ${element}`);
+//         console.log(element + '*' + outisde + '=' + element*outisde);
+//     }
+    
+// }
+
+// let myArray = ["flash", "batman", "superman"]
+// console.log(myArray.length);
+
+// for (let index = 0; index <= myArray.length; index++) {
+//     const element = myArray[index];
+//     console.log(element);
+// }
+
+// for (let i = 0; i <= 20; i++) {
+//     const element = i;
+
+//     if (element == 5) {
+//         console.log('5 is the best');
+//         break
+//     }
+//     console.log(`Value of i is ${element}`);
+    
+// }
+
+for (let i = 0; i <= 20; i++) {
+    const element = i;
+
+    if (element == 5) {
+        console.log('5 is the best');
+        continue
+    }
+    console.log(`Value of i is ${element}`);
+    
 }
+
+
+
+
+
+
 
 
 
