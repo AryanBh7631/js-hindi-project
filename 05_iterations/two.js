e5ab39b767c504457f90c0804c94949d8fe931f4
AryanBh7631 <155455129+AryanBh7631@users.noreply.github.com>
2024-01-17 06:31:47 +0000

    minor changes

--- 05_iterations/two.js
+++ 05_iterations/two.js
@@ -1,4 +1,4 @@
-let index = 0
+// let index = 0
 
 // while(index <= 10){
 
@@ -21,8 +21,74 @@ let index = 0
 //     arr = arr + 1
 // }
 
-let score = 1
+// let score = 1
 
-do {
-    console.log(`Score is ${score}`);
-} while (score <= 10);
+// do {
+//     console.log(`Score is ${score}`);
+// } while (score <= 10);
+
+
+// ********************************// ****************************************** // ****************************** //
+
+// let index = 11
+
+// do {
+//     console.log(`Value is ${index}`);
+//     index = index + 1
+// } while (index < 10);
+
+const myArray = ["four", "two", "three"]
+for (const key of myArray) {
+    console.log(key);
+}
+
+
+const myObject = {
+    name: "aryan",
+    age: 20,
+    location: "singheshwar asthan"
+}
+
+for (const key in myObject) {
+    console.log(key);
+}
+
+
+// const arr = ["pen", "pencil", "book"]
+
+// arr.forEach (function (val)) {
+//     console.log(val);
+// }
+
+const arr2 = [1, 2, 3, 4, 5]
+
+for (const samsung of arr2) {
+    console.log(samsung);
+}
+
+const namely = "aryan dotco"
+
+for (const key in namely) {
+    // if (key = "d") {
+    //     console.log("a detected");
+    // }
+    console.log(`Each greet is ${key}`);
+}
+
+const map = new Map()
+
+map.set('IN', "India")
+map.set('UN', "United Nation")
+map.set('USA', "United States of America")
+
+console.log(map);
+
+for (const [key, value] of map) {
+    console.log(key, ':-', value);
+
+}
+
+
+
+
+
