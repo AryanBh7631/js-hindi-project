1a8784dd8ed2c76594f36fd612ace3f196b50dfd
AryanBh7631 <155455129+AryanBh7631@users.noreply.github.com>
2024-01-18 15:10:10 +0000

    filter map

--- 05_iterations/six.js
+++ 05_iterations/six.js
@@ -18,5 +18,39 @@ const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 //     console.log(nums > 4);
 // });
 
-const newNums = myNums.filter( (num) => num > 4 )
-console.log(newNums);
+// const newNums = myNums.filter( (num) => num > 4 )
+// console.log(newNums);
+
+// const newNums = myNums.filter( function (val) {
+//     return val > 4
+// })
+// console.log(newNums);
+
+// const newNums = []
+
+// myNums.forEach((num) => {
+//     if (num > 4) {
+//         newNums.push(num)
+//     }
+// });
+// console.log(newNums);
+
+const books = [
+    { title: 'One', genre: 'Fiction', publish: 1981, edition: 2004},
+    { title: 'Two', genre: 'Non-Fiction', publish: 1983, edition: 2006},
+    { title: 'Three', genre: 'History', publish: 1983, edition: 2003},
+    { title: 'Four', genre: 'History', publish: 1983, edition: 2008},
+    { title: 'Five', genre: 'Economics', publish: 1972, edition: 2015}
+]
+// let UserBooks = books.filter( (bk) => bk.genre === 'History' )
+// console.log(UserBooks);
+
+
+
+const UserBooks = books.filter( (bk) => {
+   return bk.publish === 1983 && bk.edition >= 2003
+} )
+
+console.log(UserBooks);
+
+
